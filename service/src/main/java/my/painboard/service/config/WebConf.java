e7fc3047a25a480fc76bea49efea28fd2fa5131a
Yafremenka, Nadzeya <yafna01@ca.com>
2017-10-18 14:31:19 +0200

    cleanup

--- service/src/main/java/my/painboard/service/config/WebConf.java
+++ service/src/main/java/my/painboard/service/config/WebConf.java
@@ -1,15 +1,11 @@
 package my.painboard.service.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
@@ -22,42 +18,9 @@ public class WebConf extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
-//    @Bean
-//    public StringHttpMessageConverter stringHttpMessageConverter(){
-//        return new StringHttpMessageConverter();
-//    }
-//
-//    @Bean
-//    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(){
-//        return new MappingJackson2HttpMessageConverter();
-//    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("/index.html");
     }
-
-//    @Bean
-//    public DispatcherServlet dispatcherServlet() {
-//        return new DispatcherServlet();
-//    }
-
-//    @Bean
-//    public MultipartConfigElement multipartConfigElement(@Value("${upload.max_archive_size}") long maxUploadSize) {
-//        MultipartConfigFactory factory = new MultipartConfigFactory();
-//        factory.setMaxRequestSize(maxUploadSize);
-//        factory.setMaxFileSize(maxUploadSize);
-//        return factory.createMultipartConfig();
-//    }
-
-//    @Bean
-//    public ServletRegistrationBean dispatcherServletRegistration() {
-//        ServletRegistrationBean servletBean = new ServletRegistrationBean();
-//        servletBean.addUrlMappings("/*");
-////        servletBean.setMultipartConfig(multipartConfig);
-//        servletBean.setLoadOnStartup(1);
-//        servletBean.setServlet(dispatcherServlet());
-//        return servletBean;
-//
-//    }
 }
