3e4ff9613e7330a5bf1f8b9ae8e5df74b771dcb4
Stanislavk Khomytskyi <RedFlashNinja@protonmail.com>
2020-01-20 21:00:36 +0100

    configure web;

--- ui/src/main/java/my/painboard/service/config/WebConf.java
+++ ui/src/main/java/my/painboard/service/config/WebConf.java
@@ -1,36 +1,25 @@
 package my.painboard.service.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = {"my.painboard.service.controller"})
 public class WebConf implements WebMvcConfigurer {
-//    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/"};
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-//    }
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/"};
 
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName("/index.html");
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("classpath:/static/");
-        resolver.setPrefix(".html");
-        resolver.setViewNames("index");
-        return resolver;
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/index.html");
     }
 }
